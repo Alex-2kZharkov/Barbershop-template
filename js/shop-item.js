e9cbaa0224a23b2eed58e4c167c7ff3a54d40a24
Alex-2kZharkov <Alexander.06.zharkov@gmail.com>
2020-08-25 16:17:33 +0600

    The final commit with templates and active menu links. Also i linked pages between each other

--- js/shop-item.js
+++ js/shop-item.js
@@ -1,8 +1,11 @@
 let activePhoto = document.querySelector('.active-photo');
-let clickedPhoto = document.querySelectorAll('.item-img');
+let clickedPhoto = document.querySelectorAll('.img-container');
+let prevActivePhoto = null;
 
 for (let item of clickedPhoto) {
   item.addEventListener('click', () => {
-    activePhoto.setAttribute('src', item.getAttribute('src'));
+    let image = item.querySelector('.item-img');
+    activePhoto.setAttribute('src', image.getAttribute('src'));
+    item.classList.add('img-changing-animation');
   });
 }
